49d412dca5d44dc878e419e96b635b32cf463eac
JoseCabezasPok <jose.cabezas@campusdual.com>
2020-05-25 13:31:27 +0200

    Adding Base Entity

--- pet-clinic-data/src/main/java/com/example/josecabezaspetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/josecabezaspetclinic/model/Person.java
@@ -1,9 +1,17 @@
 package com.example.josecabezaspetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     public String getFirstName() {
         return firstName;
