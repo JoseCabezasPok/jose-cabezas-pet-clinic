7b063001104a48cd24f3b3fecade33fbf486454e
JoseCabezasPok <jose.cabezas@campusdual.com>
2020-05-25 15:49:04 +0200

    Add pet clinic index page and controller. closes #8.

---
+++ pet-clinic-data/src/main/java/com/example/josecabezaspetclinic/services/CrudService.java
@@ -0,0 +1,20 @@
+package com.example.josecabezaspetclinic.services;
+
+
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    T findById(ID id);
+
+    T save(T object);
+
+    Set<T> findAll();
+
+    void delete(T object);
+
+    void deleteById(ID id);
+
+
+}
--- pet-clinic-data/src/main/java/com/example/josecabezaspetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/example/josecabezaspetclinic/services/VetService.java
@@ -3,13 +3,7 @@ package com.example.josecabezaspetclinic.services;
 
 import com.example.josecabezaspetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet,Long>{
 
-public interface VetService {
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
