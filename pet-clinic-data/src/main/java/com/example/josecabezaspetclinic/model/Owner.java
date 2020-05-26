ed28af14d1b5199aced9b128960d67d544555f48
JoseCabezasPok <jose.cabezas@campusdual.com>
2020-05-26 14:02:50 +0200

    Add PetTypeService

--- pet-clinic-data/src/main/java/com/example/josecabezaspetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/josecabezaspetclinic/model/Owner.java
@@ -1,4 +1,44 @@
 package com.example.josecabezaspetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private String Telephone;
+    private String City;
+    private String address;
+    private Set<Pet> pets;
+
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        Telephone = telephone;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/com/example/josecabezaspetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.example.josecabezaspetclinic.services;
+
+import com.example.josecabezaspetclinic.model.PetType;
+import com.example.josecabezaspetclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
