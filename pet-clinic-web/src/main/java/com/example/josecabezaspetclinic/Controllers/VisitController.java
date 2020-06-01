050cdd50127dcee59df8b00f6fff6596c8b176b3
JoseCabezasPok <jose.cabezas@campusdual.com>
2020-06-01 23:18:54 +0200

    Crud operations

---
+++ pet-clinic-web/src/main/java/com/example/josecabezaspetclinic/Controllers/VisitController.java
@@ -0,0 +1,54 @@
+package com.example.josecabezaspetclinic.Controllers;
+
+
+import com.example.josecabezaspetclinic.model.Pet;
+import com.example.josecabezaspetclinic.model.Visit;
+import com.example.josecabezaspetclinic.services.PetService;
+import com.example.josecabezaspetclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Controller
+public class VisitController {
+
+   private VisitService visitService;
+   private PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder("visit")
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Map<String, Object> model){
+        Pet pet = petService.findById(petId);
+        model.put("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable("petId") Long petId, Map<String, Object> model){
+        return "pets/createOrUpdateVisitForm";
+    }
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(Visit visit, BindingResult result){
+        if(result.hasErrors()){
+            return "pets/createOrUpdateVisitForm";
+        }else {
+            this.visitService.save(visit);
+            return "redirect:/owners/{ownerId}";
+        }
+    }
+
+}
+
---
+++ pet-clinic-web/src/main/java/com/example/josecabezaspetclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,38 @@
+package com.example.josecabezaspetclinic.formatters;
+import com.example.josecabezaspetclinic.model.Pet;
+import com.example.josecabezaspetclinic.model.PetType;
+import com.example.josecabezaspetclinic.services.PetTypeService;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+
+
+    @Override
+    public PetType parse(String s, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for(PetType type : findPetTypes){
+            if(type.getName().equals(s)){
+                return type;
+            }
+        }
+        throw new ParseException("type not found: " + s, 0);
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+}
